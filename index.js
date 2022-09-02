34c295111c4a0c9397ccba9ba28283916ae6eb8e
jason mwaura mbugua <jason.mwaura@student.moringaschool.com>
2022-09-02 10:35:49 +0300

    attempted assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your classes here
+class Tree {
+    constructor( species ) {
+      this._species = species
+    }
+  
+    get species() {
+      return this._species
+      this.species = species
+    }
+  
+    static definition() {
+  class Tree {
+  class Deciduous extends Tree {
+    constructor( species, name ) {
+      super( species )
+      this._name = name
+      this.name = name
+    }
+  
+    static definition() {
+  class Deciduous extends Tree {
+  class Evergreen extends Tree {
+    constructor( species,tree) {
+      super( species )
+      this._name = name
+      this.name = name
+    }
+  
+    static definition() {
+      return `${super.definition()} Evergreens keep their leaves all year round.`
+    }
+  }
+  }
+  
